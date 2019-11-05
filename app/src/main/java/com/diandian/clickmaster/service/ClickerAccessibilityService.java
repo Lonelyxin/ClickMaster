c4edbcaa349c38a006e055ff40194699cd83854d
liuxin <liuxin@lbesec.org>
2019-11-05 19:58:04 +0800

    Initial commit

---
+++ app/src/main/java/com/diandian/clickmaster/service/ClickerAccessibilityService.java
@@ -0,0 +1,18 @@
+package com.diandian.clickmaster.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ClickerAccessibilityService extends Service {
+
+    public ClickerAccessibilityService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
